3b34168cabb3d2045ad7c378308e0b8eb5f3408c
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-16 17:31:12 +0800

    get the key from the input of user

--- cipher_text.py
+++ cipher_text.py
@@ -3,6 +3,7 @@
 # Get the message
 user_message = input('\033[1;32m Please enter your "message" in UPPERCASE LETTERS: \n')
 # Get the key
+user_key = input('Please enter your "key" also in UPPERCASE LETTERS: ')
 # Translate letter to correspond values
 # Repeat translation if not equal to length, message key
 # Add each column, excess minus 26
