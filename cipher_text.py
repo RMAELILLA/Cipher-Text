980a9020db79ff8c814fae65857933ce0929cc8a
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-16 17:27:37 +0800

    using of pseudocode

---
+++ cipher_text.py
@@ -0,0 +1,9 @@
+# Assignment 2 Problem 3 - Cipher Text
+
+# Get the message
+# Get the key
+# Translate letter to correspond values
+# Repeat translation if not equal to length, message key
+# Add each column, excess minus 26
+# convert
+# display generated cipher text
