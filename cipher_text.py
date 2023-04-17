3f64682bf86db99aea1599d90992cd21d1a18a81
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-17 09:44:56 +0800

    display generated

--- cipher_text.py
+++ cipher_text.py
@@ -10,7 +10,8 @@ def translate(string, key):
   for i in range(len(string)): 
     x = (ord(string[i]) +ord(key[i])) % 26
     x += ord('A') 
-    cipher_text.append(chr(x)) 
+    cipher_text.append(chr(x))
+  return("" . join(cipher_text))
 # Repeat translation if not equal to length, message key
 # Add each column, excess minus 26
 # convert
