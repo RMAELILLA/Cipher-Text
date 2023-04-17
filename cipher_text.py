6d2d8001920c2f2bc19420d9cc3fcb8e8b8caf45
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-17 13:10:04 +0800

    error, restart from listing

--- cipher_text.py
+++ cipher_text.py
@@ -5,22 +5,15 @@ user_message = input('\033[1;32m Please enter your "message" in UPPERCASE LETTER
 # Get the key
 user_key = input('\033[1;33m Please enter your "key" also in UPPERCASE LETTERS: \n')
 # Translate letter to correspond values
-def translate(string, key): 
-  cipher_text = [] 
-  for i in range(len(string)): 
-    x = (ord(string[i]) +ord(key[i])) % 26
-    x += ord('A') 
-    cipher_text.append(chr(x))
-  return("" . join(cipher_text))
+def translate(string, key):
+  key = list(key)
+  if len(string) == len(key):
+    return(key)
+  else:
+    for i in range(len(string) - len(key)):
+      key.append(key[i % len(key)])
+  return("" . join(key))
 # Repeat translation if not equal to length, message key
-def rpt_translate(string, key):
-    key = list(key)
-    if len(string) == len(key):
-        return(key)
-    else:
-        for i in range(len(string) - len(key)):
-            key.append(key[i % len(key)])
-    return("" . join(key))
 # Add each column, excess minus 26
 # convert
 if __name__ == "__main__":
